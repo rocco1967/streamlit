1e11b99d7c7336315d9213e3a65fad6e8a5504cc
rocco1967 <69043216+rocco1967@users.noreply.github.com>
2022-09-29 13:30:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def predict(Gender, Age, Height, Weight, Duration, Heart_Rate,Body_Temp):
     return prediction
 image = Image.open('dottore.png.png')
 st.image(image)
-st.title('Calcolo Calorie')
+st.title('       Calcolo Calorie')
 #st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
 
 st.header('     Immetti i dati:  attenzione digita in Gender 0 =Donna  1= Uomo')
