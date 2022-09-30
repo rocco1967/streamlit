e82e023276349cb35806c1870fb8bb8e9360b1ce
rocco1967 <69043216+rocco1967@users.noreply.github.com>
2022-09-30 11:00:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ image = Image.open('dottore.png.png')
 image2= Image.open('allenamento2.jpg')
 st.image(image2)
 #st.image(image)
-st.title('..........Calcolo Calorie........')
+st.title('.............Calcolo Calorie...........')
 #st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
 
 #st.header('     Immetti i dati:')
